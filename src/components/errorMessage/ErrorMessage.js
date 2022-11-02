7a5ef8d3a6d6b4977d948655d91d76ed7ab5f552
vastin1 <tim.milashich@gmail.com>
2022-11-02 20:48:09 +0200

    Updates

--- src/components/errorMessage/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import img from './error.png'
-
-const ErrorMessage = () => {
-  return(
-    <img src={img} style={{margin: '0 auto', height: "250px"}}  alt='Error'/>
-  )
-}
-
-export default ErrorMessage;
